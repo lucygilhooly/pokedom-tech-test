eb63d6d81a7568ee9f0417490197a5705ef7ab53
lucy gilhooly <lucy_johannah@hotmail.com>
2022-08-23 19:18:16 +0100

    updated the content

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ import {
 
 const getPokemon = ((pokemon) => {
     return `<div class="card">
-            <img class="card__sprite card__image" src="${pokemon.sprite}">
-            <h1 class="card__name card__heading"> ${pokemon.name}</h1>
-            <div class="card__id card__content">${pokemon.id}</div>
-            <p class="card__types card__content">${pokemon.types.join(", ")}</p>
+            <img class="card__image" src="${pokemon.sprite}">
+            <h1 class="card__heading"> ${pokemon.name}</h1>
+            <p class="card__content">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types} ${pokemon.type}</p>
             </div>`
 })
 
